e1af46bba75a655d2e90900c196655a730ac1293
Matthew Carlson <108031642+matthewctechnology@users.noreply.github.com>
2025-10-29 23:55:55 +0000

    configures eslint and jest with globals

--- critique/jest.config.js
+++ critique/jest.config.js
@@ -3,12 +3,12 @@ const nextJest = require('next/jest')
 const createJestConfig = nextJest({ dir: './' })
 
 const customJestConfig = {
-    transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    testMatch: ['**/?(*.)+(test).+(ts|tsx|js)'],
+  transform: {
+      '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/?(*.)+(test).+(ts|tsx|js)'],
 }
 
 module.exports = createJestConfig(customJestConfig)
