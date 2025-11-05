b6c32594b4c41ec425c6959b3b4cf36a57de7753
Matthew Carlson <108031642+matthewctechnology@users.noreply.github.com>
2025-11-05 18:45:45 +0000

    moves tests to critique test app directory outside of src

--- critique/test/app/page.test.tsx
+++ critique/test/app/page.test.tsx
@@ -1,5 +1,5 @@
 import { describe, expect, test } from '@jest/globals';
-import Home from './page';
+import Home from '../../src/app/page';
 import { fireEvent, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
