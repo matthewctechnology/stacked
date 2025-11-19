0977324f9637e963f62f5bd5f745f3cb967df96a
Matthew Carlson <108031642+matthewctechnology@users.noreply.github.com>
2025-11-19 18:30:30 +0000

    configures eslint with next and repairs react misconfiguration

--- critique/eslint.config.mjs
+++ critique/eslint.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from "eslint/config";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import globals from "globals";
+import { defineConfig } from 'eslint/config';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import globals from 'globals';
+import plugin from 'eslint-plugin-react';
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -13,14 +14,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = defineConfig([
-  ...compat.extends("next/core-web-vitals", "next/typescript", "next"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'next'),
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
     ],
     files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
     languageOptions: {
@@ -29,9 +30,21 @@ const eslintConfig = defineConfig([
         ...globals.jest,
       },
     },
+    plugins: {
+      react: plugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+        pragma: 'React',
+        jsxPragma: 'React',
+        runtime: 'automatic',
+      },
+    },
     rules: {
       'no-unused-vars': 'warn',
-      'no-undef': 'warn',
+      'no-undef': 'off',
+      'react/react-in-jsx-scope': 'off',
     },
   },
 ]);
