50310139b5bbfb356ff3c9238d95c225f9d542df
Matthew Carlson <108031642+matthewctechnology@users.noreply.github.com>
2025-11-21 22:20:20 +0000

    starts critique from minor version zero and scripts version release

--- critique/scripts/version.sh
+++ critique/scripts/version.sh
@@ -8,7 +8,7 @@ if [[ "$CURRENT_BRANCH" == "main" ]]; then
 fi
 
 if [[ "$#" -ne 1 ]]; then
-  echo 'Usage: ./scripts/version.sh [patch|minor|major|same]'
+  echo 'Usage: ./scripts/version.sh [patch|minor|major]'
   exit 1
 fi
 
@@ -16,23 +16,17 @@ VERSION_TYPE="$1"
 if [[ "$VERSION_TYPE" != "patch"
   && "$VERSION_TYPE" != "minor"
   && "$VERSION_TYPE" != "major"
-  && "$VERSION_TYPE" != "same"
 ]]; then
-  echo 'Error: Version types: patch, minor, major, or same.'
+  echo 'Error: Version types: patch, minor, major.'
   exit 1
 fi
 
 pushd "$(dirname "$0")/.." > /dev/null
-if [[ "$VERSION_TYPE" == "same" ]]; then
-  CURRENT_VERSION=$(npm pkg get version | tr -d '"')
-  NEW_VERSION=$(npm version "$CURRENT_VERSION" --no-git-tag-version --allow-same-version | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+')
-else
-  pushd "$(dirname "$0")/.." > /dev/null
-  NEW_VERSION=$(npm version "$VERSION_TYPE" --no-git-tag-version | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+')
-fi
+NEW_VERSION=$(npm version "$VERSION_TYPE" --no-git-tag-version | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+')
 popd > /dev/null
 
-git add critique/package.json
+git add critique/package.json critique/package-lock.json
 git commit -m "Version critique v$NEW_VERSION"
 git tag "v$NEW_VERSION" -m "Release critique v$NEW_VERSION"
 git push origin "$CURRENT_BRANCH:$CURRENT_BRANCH" "v$NEW_VERSION"
+
