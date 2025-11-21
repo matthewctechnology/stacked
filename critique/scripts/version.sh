7d89130a524578b919675c83210bf1e7255af8c3
Matthew Carlson <108031642+matthewctechnology@users.noreply.github.com>
2025-11-21 22:40:40 +0000

    scripts critique app version release to run from stacked sub directories

--- critique/scripts/version.sh
+++ critique/scripts/version.sh
@@ -26,8 +26,7 @@ NEW_VERSION=$(npm version "$VERSION_TYPE" --no-git-tag-version | grep -o '[0-9]\
 
 pushd "$(dirname "$1")/.." > /dev/null
 git add critique/package.json critique/package-lock.json
-popd > /dev/null
-
 git commit -m "Version critique v$NEW_VERSION"
 git tag "v$NEW_VERSION" -m "Release critique v$NEW_VERSION"
 git push origin "$CURRENT_BRANCH:$CURRENT_BRANCH" "v$NEW_VERSION"
+popd > /dev/null
