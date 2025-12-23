492e8430caf5a5123cdc5f9c21bcd290dcc0cf5b
Matthew Carlson <108031642+matthewctechnology@users.noreply.github.com>
2025-12-23 20:00:00 +0000

    provides various fallback responses with critique app

--- critique/test/app/fallbackResonses.test.ts
+++ critique/test/app/fallbackResonses.test.ts
@@ -1,10 +1,16 @@
 import { fallbackResponseProvider } from '../../src/app/fallbackResponses';
-import { describe, expect, test } from '@jest/globals';
+import { describe, jest, expect, test } from '@jest/globals';
 
 
 describe('fallbackResponseProvider', () => {
   test('returns fallback response', () => {
     const response = fallbackResponseProvider.getResponse();
-    expect(response).toMatch(/critique/i);
+    expect(response).not.toBeNull();
+  });
+
+  test('returns fallback error response', () => {
+    fallbackResponseProvider.getResponse = jest.fn(fallbackResponseProvider.getResponse).mockReturnValue('Unable to critique because of Error.');
+    const response = fallbackResponseProvider.getResponse();
+    expect(response).toMatch(/Unable to critique/i);
   });
 });
