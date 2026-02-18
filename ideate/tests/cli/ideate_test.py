55bfed0c3ec60171454e3aa79fbe749d44659599
Matthew Carlson <108031642+matthewctechnology@users.noreply.github.com>
2026-02-18 20:00:00 +0000

    prints predetermined idea with ideate cli via typer

--- ideate/tests/cli/ideate_test.py
+++ ideate/tests/cli/ideate_test.py
@@ -1,14 +1,33 @@
 """
 Tests CLI entrypoint for ideate tool.
 """
-from ideate.cli.ideate import ideate
+from typer.testing import CliRunner
+from ideate.cli.ideate import app
 from ideate.fallback.fallback_responses import responses
 
+runner = CliRunner()
 
-def test_ideate() -> None:
+def test_ideate_cli() -> None:
     """
-    Tests ideate returns a valid fallback idea.
+    Tests ideate CLI prints a valid fallback idea.
     """
-    idea = ideate()
+    result = runner.invoke(app)
 
-    assert idea in responses
+    assert result.exit_code == 0
+    assert result.output.strip() in responses
+
+def test_ideate_cli_help() -> None:
+    """
+    Tests ideate CLI help.
+    """
+    result = runner.invoke(app, ["--help"])
+
+    assert result.exit_code == 0
+
+def test_ideate_cli_fail() -> None:
+    """
+    Tests ideate CLI failure.
+    """
+    result = runner.invoke(app, ["ideate"])
+
+    assert result.exit_code == 2
